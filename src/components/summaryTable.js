ee78a7557c53deb328f63de5d21791184013c6e5
unknown <dmt5dh@virginia.edu>
2020-04-02 00:06:14 -0400

    clean up stuff for build

--- src/components/summaryTable.js
+++ src/components/summaryTable.js
@@ -1,21 +1,31 @@
 import React from "react"
 import DataTable from "react-data-table-component"
 
-const SummaryTable = ({ data, title, columns, defaultSortField, keyField }) => (
-  <div className="mx-8">
-    <h1 className="underline text-center text-2xl">{title}</h1>
-    <DataTable
-      noHeader={true}
-      columns={columns}
-      data={data}
-      defaultSortField={defaultSortField}
-      defaultSortAsc={false}
-      keyField={keyField}
-      striped={true}
-      highlightOnHover={true}
-      pagination={true}
-    />
-  </div>
-)
+const SummaryTable = ({ data, title, columns, defaultSortField, keyField }) => {
+
+    //react-data-table-component relies on document which we'll skip during server side rendering on build
+    if(typeof document === `undefined`) {
+        return (
+            null
+        )
+    }
+
+    return (
+        <div className="mx-8">
+            <h1 className="underline text-center text-2xl">{title}</h1>
+            <DataTable
+            noHeader={true}
+            columns={columns}
+            data={data}
+            defaultSortField={defaultSortField}
+            defaultSortAsc={false}
+            keyField={keyField}
+            striped={true}
+            highlightOnHover={true}
+            pagination={true}
+            />
+        </div>
+    )
+}
 
 export default SummaryTable
