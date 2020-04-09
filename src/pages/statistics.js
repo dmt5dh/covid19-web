dc99004d6c7552a10105a2fe25143a6a4ea29499
unknown <dmt5dh@virginia.edu>
2020-04-09 16:07:19 -0400

    refactor pages with apollo calls for proper gatsby builds

--- src/pages/statistics.js
+++ src/pages/statistics.js
@@ -1,36 +1,15 @@
-import React, { setState } from "react"
-import ApolloClient from "apollo-boost"
-import { gql } from "apollo-boost"
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import moment from "moment"
 
+import StatsLineChart from "../components/statsLineChart"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import StatsLineChart from "../components/statsLineChart"
 
-// const data = [
-//     {name: '22 Jan', uv: 400, pv: 100, amt: 2400},
-//     {name: '23 Jan', uv: 300, pv: 400, amt: 2400},
-//     {name: '24 Jan', uv: 500, pv: 200, amt: 2400},
-//     {name: '25 Jan', uv: 900, pv: 50, amt: 2400},
-// ]
-const client = new ApolloClient({
-  uri: "https://covid19-graphql.now.sh/",
-})
-
-const today = moment()
-  .add(-1, "days")
-  .format("LLL")
-var query
-class StatisticsPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      data: {},
-      loading: true,
-    }
-
-    query = gql`
+const today = moment().format("LLL")
+const StatisticsPage = (props) => {
+    const query = gql`
       query($country: String) {
         country(name: $country) {
           name
@@ -44,38 +23,24 @@ class StatisticsPage extends React.Component {
         }
       }
     `
-  }
 
-  componentDidMount() {
-    client
-      .query({
-        query: query,
-        variables: {
-          country: decodeURI(this.props.location.search.split("=")[1]),
-        },
-      })
-      .then(response => {
-        this.setState({
-          countryName: response.data.country.name,
-          data: response.data.country.results,
-          loading: false,
-        })
-      })
-  }
-
-  render() {
-    return (
-      <Layout>
-        <SEO title="Country Statistics" />
-        <div>
-          {this.state.loading ? (
+    const { loading, error, data } = useQuery(query, {variables: { country: decodeURI(props.location.search.split("=")[1]) }});
+    return(
+        <Layout>
+            <SEO title="Country Statistics" />
+            {loading && 
             <div className="text-center">
-              <h1 className="text-4xl">Retrieving Data...</h1>
-            </div>
-          ) : (
-            <div className="flex flex-col">
+            <h1 className="text-4xl">Retrieving Data...</h1>
+          </div>
+            }
+
+            {data &&
+            // <div>
+            //   {JSON.stringify(data)}
+            // </div>
+                <div className="flex flex-col">
               <h1 className="mx-auto text-2xl underline mb-2">
-                Country data for {this.state.countryName}
+                Country data for {decodeURI(props.location.search.split("=")[1])}
               </h1>
               <p className="mx-auto text-gray-500 sm:text-sm text-xs">
                 Data on {today}. Updated daily
@@ -91,34 +56,31 @@ class StatisticsPage extends React.Component {
                 <tbody>
                   <tr>
                     <td className="border px-4 py-2 text-center">
-                      {this.state.data[
-                        this.state.data.length - 1
+                      {data.country.results[
+                        data.country.results.length - 1
                       ].confirmed.toLocaleString("en")}
                     </td>
                     <td className="border px-4 py-2 text-center">
-                      {this.state.data[
-                        this.state.data.length - 1
+                      {data.country.results[
+                        data.country.results.length - 1
                       ].deaths.toLocaleString("en")}
                     </td>
                     <td className="border px-4 py-2 text-center">
-                      {this.state.data[
-                        this.state.data.length - 1
+                      {data.country.results[
+                        data.country.results.length - 1
                       ].recovered.toLocaleString("en")}
                     </td>
                   </tr>
                 </tbody>
               </table>
               <StatsLineChart
-                data={this.state.data}
+                data={data.country.results}
                 minHeight={475}
                 width={"100%"}
               />
             </div>
-          )}
-        </div>
-      </Layout>
+            }
+        </Layout>
     )
-  }
 }
-
 export default StatisticsPage
