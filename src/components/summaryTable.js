2cfa51c3705d75dea2e044c3f199ea99d0cea3fe
unknown <dmt5dh@virginia.edu>
2020-04-13 13:15:32 -0400

    update padding to show all of table on mobile

--- src/components/summaryTable.js
+++ src/components/summaryTable.js
@@ -50,7 +50,7 @@ class SummaryTable extends React.Component {
     }
 
     return (
-      <div className="md:mx-32 mx-8">
+      <div className="md:mx-32 mx-8 my-4">
         <div className="w-full flex justify-between">
           <h1 className="underline text-md sm:text-xl mb-1">
             {this.props.title}
