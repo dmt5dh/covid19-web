a787efbf6d0e166d58d5191b2846f405c328eaac
unknown <dmt5dh@virginia.edu>
2020-04-13 13:36:00 -0400

    sticky header

--- src/components/header.js
+++ src/components/header.js
@@ -2,27 +2,30 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Search from "./search/search"
+
 const Header = ({ siteTitle }) => (
-  <nav className="w-full flex items-center justify-center flex-wrap bg-indigo-700 p-4 md:p-6 text-xl">
-    <div className="mx-8 flex">
+  <nav className="w-full bg-indigo-700 p-4 md:p-6 text-xl sticky top-0 z-50">
+    <div className="xl::px-24 sm:mx-16 xl:mx-64 flex justify-around">
       <Link
         to="/"
-        className="md:text-base text-sm text-white hover:text-gray-300 mr-10"
+        className="md:text-base text-sm text-white hover:text-indigo-300 mr-10"
       >
         Summary
       </Link>
       <Link
         to="/map"
-        className="md:text-base text-sm text-white hover:text-gray-300 mr-10"
+        className="md:text-base text-sm text-white hover:text-indigo-300 mr-10"
       >
         Map
       </Link>
       <Link
         to="/about"
-        className="md:text-base text-sm text-white hover:text-gray-300"
+        className="md:text-base text-sm text-white hover:text-indigo-300"
       >
-        About the data
+        About
       </Link>
+      <Search />
     </div>
   </nav>
 )
